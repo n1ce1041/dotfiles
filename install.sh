ed0d380ed8e8fa963cf6a60f9c0ac27c808b8bc8
Vili <viliamiktutone@gmail.com>
2024-12-19 17:22:59 +1300

    Added vim_runtime

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copying vim to home dir
+
+sudo cp -r .vim_runtime/ ~/
+
 # Symlinks for dotfiles
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.fishrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
